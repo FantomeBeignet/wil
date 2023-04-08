63b5d57e1aa9dc5c8b4667745e54dd385a390a70
Tom Béné <himself@fantomebeig.net>
2023-04-08 16:04:01 +0200

    feat: all reactions

--- src/listeners/react.ts
+++ src/listeners/react.ts
@@ -1,6 +1,6 @@
 import { ApplyOptions } from '@sapphire/decorators';
 import { Listener, ListenerOptions } from '@sapphire/framework';
-import type { GuildEmoji, Message, TextChannel } from 'discord.js';
+import type { GuildEmoji, Message, MessageReaction, TextChannel } from 'discord.js';
 import { isTextChannel } from '../lib/utils';
 
 @ApplyOptions<ListenerOptions>({})
@@ -13,13 +13,17 @@ export class QuoiEvent extends Listener {
 		});
 	}
 
-	public run(message: Message): Promise<void> | undefined | null {
+	public run(message: Message): Promise<Message | MessageReaction> | Promise<void> | undefined | null {
 		if ((message.channel as TextChannel).parent?.id !== process.env.SERIOUS_CATEGORY && message.author.id !== process.env.CLIENT_ID) {
-			const { content } = message;
+			const { content, mentions } = message;
 			const words = content.toLowerCase().split(' ');
 			if (words.includes('ratio')) return this.react(message, 'ratio');
 			if (words.includes('cheh')) return this.react(message, 'orge');
 			if (words.includes('titre')) return this.react(message, 'titre');
+			if (mentions.roles.has('1028963968309526618')) return message.react('🤝');
+			if (mentions.roles.has('1028958078101631046'))
+				return message.reply({ content: 'Mais qui est William ?', allowedMentions: { repliedUser: false } });
+			if (mentions.roles.has('1029027447318003764')) return this.react(message, 'orge');
 		}
 		return null;
 	}
