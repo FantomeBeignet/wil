a4178ff7a3de0fb6cffe57862d8df7fb2e48b053
Tom Béné <himself@fantomebeig.net>
2023-04-08 17:04:30 +0200

    feat: unregister commands on startup

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { LogLevel, SapphireClient } from '@sapphire/framework';
 import { Logger } from '@sapphire/plugin-logger';
 import '@sapphire/plugin-logger/register';
-import { GatewayIntentBits } from 'discord.js';
+import { GatewayIntentBits, REST, Routes } from 'discord.js';
 
 const client = new SapphireClient({
 	caseInsensitiveCommands: true,
@@ -14,6 +14,10 @@ const client = new SapphireClient({
 
 const main = async () => {
 	try {
+		const rest = new REST({ version: '1.0' }).setToken(process.env.DISCORD_TOKEN as string);
+		rest.put(Routes.applicationCommands(process.env.CLIENT_ID as string), { body: [] })
+			.then(() => console.log('Successfully deleted all application commands.'))
+			.catch(console.error);
 		client.logger.info('Logging in');
 		await client.login(process.env.DISCORD_TOKEN);
 		client.logger.info('Logged in');
