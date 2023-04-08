42187981256c03d46c5ef9e0d45e1da5f99740d0
Tom Béné <himself@fantomebeig.net>
2023-04-08 16:45:00 +0200

    feat: quote

--- src/commands/quote.ts
+++ src/commands/quote.ts
@@ -1,21 +1,38 @@
 import { ChatInputCommand, Command } from '@sapphire/framework';
 import { ApplyOptions } from '@sapphire/decorators';
+import { makeQuoteEmbed } from '../lib/utils';
+import type { TextChannel } from 'discord.js';
 
 @ApplyOptions<Command.Options>({
-	name: 'ping',
-	description: 'Pong?'
+	name: 'quote',
+	description: "C'est ici le vrai Mini Tel'"
 })
-export class PingCommand extends Command {
+export class QuoteCommand extends Command {
 	public constructor(context: Command.Context, options: Command.Options) {
 		super(context, { ...options });
 	}
 
 	public override registerApplicationCommands(registry: ChatInputCommand.Registry) {
-		registry.registerChatInputCommand((builder) => builder.setName(this.name).setDescription(this.description));
+		registry.registerChatInputCommand((builder) =>
+			builder
+				.setName(this.name)
+				.setDescription(this.description)
+				.addUserOption((option) => option.setName('author').setDescription("L'auteur de la dingz").setRequired(true))
+				.addStringOption((option) => option.setName('content').setDescription('La dingz en question').setRequired(true))
+		);
 	}
 
 	public async chatInputRun(interaction: Command.ChatInputCommandInteraction) {
-		const msg = await interaction.reply({ content: 'Pong!' });
-		return msg;
+		const author = interaction.options.getUser('author');
+		const content = interaction.options.getString('content')!;
+		const usedBy = interaction.user;
+		const title = 'A dit :';
+		const embed = makeQuoteEmbed(usedBy, title, author, content);
+		const quoteChannel = interaction.guild?.channels.cache.get(process.env.QUOTE_CHANNEL as string) as TextChannel;
+		await quoteChannel.send({ embeds: [embed] });
+		await interaction.reply({
+			content: 'Quote envoyée !',
+			ephemeral: true
+		});
 	}
 }
