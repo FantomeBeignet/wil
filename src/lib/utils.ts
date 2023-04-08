99cbca2b04fe6ba76ff9db734bc98047419c3cca
Tom Béné <himself@fantomebeig.net>
2023-04-08 16:44:51 +0200

    feat: function to make quote embed

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,22 @@
-import { Channel, TextChannel } from 'discord.js';
+import { Channel, EmbedBuilder, TextChannel, User } from 'discord.js';
 
 export const isTextChannel = (channel: Channel): channel is TextChannel => {
 	return channel instanceof TextChannel;
+};
+
+export const makeQuoteEmbed = (usedBy: User, title: string, author: User | null, content: string) => {
+	const embed = new EmbedBuilder()
+		.setTitle(title)
+		.setDescription(content)
+		.setTimestamp()
+		.setFooter({
+			text: `Envoyé par ${usedBy.username}`
+		});
+	if (author) {
+		embed.setAuthor({
+			name: author.username,
+			iconURL: author.displayAvatarURL()
+		});
+	}
+	return embed;
 };
