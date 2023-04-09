b8e662833e8c335bafcc2a2c9dedf92679dbcf88
Tom Béné <himself@fantomebeig.net>
2023-04-09 13:23:07 +0200

    feat: all quote types

--- src/commands/quote.ts
+++ src/commands/quote.ts
@@ -36,3 +36,131 @@ export class QuoteCommand extends Command {
 		});
 	}
 }
+
+@ApplyOptions<Command.Options>({
+	name: 'vu',
+	description: 'Quand tu vois un truc (duh)'
+})
+export class VuCommand extends Command {
+	public constructor(context: Command.Context, options: Command.Options) {
+		super(context, { ...options });
+	}
+
+	public override registerApplicationCommands(registry: ChatInputCommand.Registry) {
+		registry.registerChatInputCommand((builder) =>
+			builder
+				.setName(this.name)
+				.setDescription(this.description)
+				.addStringOption((option) => option.setName('content').setDescription('La dingz vue').setRequired(true))
+		);
+	}
+
+	public async chatInputRun(interaction: Command.ChatInputCommandInteraction) {
+		const content = interaction.options.getString('content')!;
+		const usedBy = interaction.user;
+		const title = 'Vu :';
+		const embed = makeQuoteEmbed(usedBy, title, null, content);
+		const quoteChannel = interaction.guild?.channels.cache.get(process.env.QUOTE_CHANNEL as string) as TextChannel;
+		await quoteChannel.send({ embeds: [embed] });
+		await interaction.reply({
+			content: 'Quote envoyée !',
+			ephemeral: true
+		});
+	}
+}
+
+@ApplyOptions<Command.Options>({
+	name: 'entendu',
+	description: 'Quand tu entendu un truc (duh)'
+})
+export class EntenduCommand extends Command {
+	public constructor(context: Command.Context, options: Command.Options) {
+		super(context, { ...options });
+	}
+
+	public override registerApplicationCommands(registry: ChatInputCommand.Registry) {
+		registry.registerChatInputCommand((builder) =>
+			builder
+				.setName(this.name)
+				.setDescription(this.description)
+				.addStringOption((option) => option.setName('content').setDescription('La dingz entendue').setRequired(true))
+		);
+	}
+
+	public async chatInputRun(interaction: Command.ChatInputCommandInteraction) {
+		const content = interaction.options.getString('content')!;
+		const usedBy = interaction.user;
+		const title = 'Entendu :';
+		const embed = makeQuoteEmbed(usedBy, title, null, content);
+		const quoteChannel = interaction.guild?.channels.cache.get(process.env.QUOTE_CHANNEL as string) as TextChannel;
+		await quoteChannel.send({ embeds: [embed] });
+		await interaction.reply({
+			content: 'Quote envoyée !',
+			ephemeral: true
+		});
+	}
+}
+
+@ApplyOptions<Command.Options>({
+	name: 'pasvu',
+	description: 'Quand tu vois pas un truc (duh)'
+})
+export class PasVuCommand extends Command {
+	public constructor(context: Command.Context, options: Command.Options) {
+		super(context, { ...options });
+	}
+
+	public override registerApplicationCommands(registry: ChatInputCommand.Registry) {
+		registry.registerChatInputCommand((builder) =>
+			builder
+				.setName(this.name)
+				.setDescription(this.description)
+				.addStringOption((option) => option.setName('content').setDescription('La dingz pas vue').setRequired(true))
+		);
+	}
+
+	public async chatInputRun(interaction: Command.ChatInputCommandInteraction) {
+		const content = interaction.options.getString('content')!;
+		const usedBy = interaction.user;
+		const title = 'Pas vu :';
+		const embed = makeQuoteEmbed(usedBy, title, null, content);
+		const quoteChannel = interaction.guild?.channels.cache.get(process.env.QUOTE_CHANNEL as string) as TextChannel;
+		await quoteChannel.send({ embeds: [embed] });
+		await interaction.reply({
+			content: 'Quote envoyée !',
+			ephemeral: true
+		});
+	}
+}
+
+@ApplyOptions<Command.Options>({
+	name: 'pasentendu',
+	description: 'Quand tu entends pas un truc (duh)'
+})
+export class PasEntenduCommand extends Command {
+	public constructor(context: Command.Context, options: Command.Options) {
+		super(context, { ...options });
+	}
+
+	public override registerApplicationCommands(registry: ChatInputCommand.Registry) {
+		registry.registerChatInputCommand((builder) =>
+			builder
+				.setName(this.name)
+				.setDescription(this.description)
+				.addStringOption((option) => option.setName('content').setDescription('La dingz pas entendu').setRequired(true))
+		);
+	}
+
+	public async chatInputRun(interaction: Command.ChatInputCommandInteraction) {
+		const content = interaction.options.getString('content')!;
+		const usedBy = interaction.user;
+		const title = 'Pas entendu :';
+		const embed = makeQuoteEmbed(usedBy, title, null, content);
+		const quoteChannel = interaction.guild?.channels.cache.get(process.env.QUOTE_CHANNEL as string) as TextChannel;
+		await quoteChannel.send({ embeds: [embed] });
+		await interaction.reply({
+			content: 'Quote envoyée !',
+			ephemeral: true
+		});
+	}
+}
