30d6189b7ca30761e8d53b29a780ff3c24421242
Tom Béné <himself@fantomebeig.net>
2023-04-09 15:26:28 +0200

    fix: vote counting

--- src/listeners/council.ts
+++ src/listeners/council.ts
@@ -17,31 +17,28 @@ export class CouncilVoteEvent extends Listener {
 	public async run(interaction: Interaction) {
 		if (!interaction.isButton()) return null;
 		if (interaction.customId.startsWith('council')) {
+			const voterId = interaction.user.id;
 			const voteId = interaction.message.id;
-			console.log('voting for vote', voteId);
 			const [, mode] = interaction.customId.split('_');
 			const author = interaction.message.embeds[0].author as {
 				name: string;
 				iconURL: string;
 			};
 			const propal = interaction.message.embeds[0].description as string;
-			let [votesFor, votesAgainst] = interaction.message.components[0].components.map((button) =>
-				Number.parseInt(button.customId?.split('_')[2] as string, 10)
-			);
 			switch (mode) {
 				case 'plus':
-					if (await redisClient.sismember(`votes:for:${voteId}`, author.name)) return null;
-					votesFor += 1;
-					if (await redisClient.smove(`votes:against:${voteId}`, `votes:for:${voteId}`, author.name)) votesAgainst -= 1;
-					else await redisClient.sadd(`votes:for:${voteId}`, author.name);
+					if (await redisClient.sismember(`votes:for:${voteId}`, voterId)) return null;
+					if (!(await redisClient.smove(`votes:against:${voteId}`, `votes:for:${voteId}`, voterId)))
+						await redisClient.sadd(`votes:for:${voteId}`, voterId);
 					break;
 				case 'minus':
-					if (await redisClient.sismember(`votes:against:${voteId}`, author.name)) return null;
-					votesAgainst += 1;
-					if (await redisClient.smove(`votes:for:${voteId}`, `votes:against:${voteId}`, author.name)) votesFor -= 1;
-					else await redisClient.sadd(`votes:against:${voteId}`, author.name);
+					if (await redisClient.sismember(`votes:against:${voteId}`, voterId)) return null;
+					if (!(await redisClient.smove(`votes:for:${voteId}`, `votes:against:${voteId}`, voterId)))
+						await redisClient.sadd(`votes:against:${voteId}`, voterId);
 					break;
 			}
+			const votesFor = await redisClient.scard(`votes:for:${voteId}`);
+			const votesAgainst = await redisClient.scard(`votes:against:${voteId}`);
 			return interaction.update(makeCouncilEmbed(propal, author, votesFor, votesAgainst));
 		}
 		return null;
