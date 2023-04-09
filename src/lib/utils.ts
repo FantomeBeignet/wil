e59368c02a7fe265f657a482efa0c89185820b75
Tom Béné <himself@fantomebeig.net>
2023-04-09 14:29:04 +0200

    feat: builder for council embed

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,4 @@
-import { Channel, EmbedBuilder, TextChannel, User } from 'discord.js';
+import { ActionRowBuilder, ButtonBuilder, ButtonStyle, Channel, Colors, EmbedBuilder, TextChannel, User } from 'discord.js';
 
 export const isTextChannel = (channel: Channel): channel is TextChannel => {
 	return channel instanceof TextChannel;
@@ -20,3 +20,21 @@ export const makeQuoteEmbed = (usedBy: User, title: string, author: User | null,
 	}
 	return embed;
 };
+
+export function makeCouncilEmbed(propal: string, author: { name: string; iconURL: string }, votesFor: number, votesAgainst: number) {
+	const embed = new EmbedBuilder()
+		.setTitle('Proposition pour le conseil')
+		.setAuthor(author)
+		.setDescription(propal)
+		.addFields(
+			{ name: 'Votes pour :', value: votesFor.toString(), inline: true },
+			{ name: 'Votes contre :', value: votesAgainst.toString(), inline: true }
+		)
+		.setTimestamp();
+	votesFor > votesAgainst ? embed.setColor(Colors.Green) : votesFor < votesAgainst ? embed.setColor(Colors.Red) : embed.setColor(Colors.Default);
+	const buttons = new ActionRowBuilder<ButtonBuilder>().addComponents(
+		new ButtonBuilder().setCustomId(`council_plus_${votesFor.toString()}`).setEmoji('✔️').setStyle(ButtonStyle.Success),
+		new ButtonBuilder().setCustomId(`council_minus_${votesAgainst.toString()}`).setEmoji('✖️').setStyle(ButtonStyle.Danger)
+	);
+	return { embeds: [embed], components: [buttons] };
+}
