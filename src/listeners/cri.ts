6b69b5cba42a7b503f61889c814480d209e3ce36
Guillaume RICARD <92726277+CitelisIT@users.noreply.github.com>
2023-07-13 13:38:01 +0000

    fix: correctly screams

--- src/listeners/cri.ts
+++ src/listeners/cri.ts
@@ -53,7 +53,8 @@ export class CriEvent extends Listener {
 				if (index !== -1) {
 					// if word includes "cri" or "cry"
 					if (!this.conjugations.includes(word)) {
-						const toReply = word.slice(index + 3);
+						const bold = require("discord.js");
+						const toReply = bold(word.slice(index + 3));
 						if (toReply.length > 4 && toReply.length < 12) {
 							return message.reply({ content: toReply, allowedMentions: { repliedUser: false } });
 						}
